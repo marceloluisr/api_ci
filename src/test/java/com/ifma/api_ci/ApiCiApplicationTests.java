215671e5e496aca48e8a044e730a3db0f7402154
marceloluisr <marceloluis.pessoal@gmail.com>
2021-04-22 16:38:07 -0300

    first commit

---
+++ src/test/java/com/ifma/api_ci/ApiCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifma.api_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
